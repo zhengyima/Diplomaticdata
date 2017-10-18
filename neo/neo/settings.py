53fc2d3ac9a4767c4d17a9ff238f8fa8f799e380
lmerengues <317958662@qq.com>
2017-10-18 12:59:58 +0800

    remove neomodel

--- neo/neo/settings.py
+++ neo/neo/settings.py
@@ -12,13 +12,11 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 
-from neomodel import config
 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-config.DATABASE_URL = 'http://127.0.0.1:7474'  # default
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
